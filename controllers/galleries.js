e7ce72486f6b680e2992665c19b1e09b0268f3c9
nickosp <nickospapageorge85@gmail.com>
2020-11-17 18:22:24 -0500

    added post

--- controllers/galleries.js
+++ controllers/galleries.js
@@ -1,15 +1,25 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
-const Galleries = require('../models/gallery')
+const Galleries = require('../models/gallery');
 
 // Get all galleries
 // http://localhost:5000/api/galleries
 router.get('/', (req, res, next) => {
-    Galleries.find()
-    .then(galleries => res.json(galleries))
-    .catch(next)
-})
+	Galleries.find()
+		.then((galleries) => res.json(galleries))
+		.catch(next);
+});
 
-module.exports = router
+router.post('/', (req, res, next) => {
+	Galleries.create(req.body)
+		.then((gallery) => {
+			Galleries.find({}).then((galleries) => {
+				res.json(galleries);
+			});
+		})
+		.catch(next);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use('/api/gallerytitles', galleryTitlesControllers);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`App is running ${port}`);
+	console.log(`App is running ${port}!`);
 });
