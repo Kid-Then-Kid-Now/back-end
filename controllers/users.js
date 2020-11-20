a0299504e4a6efb9ac5f1c68b37e13fd4015667a
nickosp <nickospapageorge85@gmail.com>
2020-11-20 18:54:34 -0500

    added bcrypt to hash passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
@@ -7,7 +8,18 @@ const router = express.Router();
 
 // SIGN UP
 // POST /api/signup
-router.post('/signup', (req, res, next) => {});
+router.post('/signup', async (req, res, next) => {
+    // wrap it in a try
+  try {
+    // store the results of any asynchronous calls in variables
+    const password = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({ email: req.body.email, password })
+    res.status(201).json(user) 
+  } catch (error) {
+		// return the next callback and pass it the error from catch
+		return next(error);
+	}
+});
 
 // SIGN IN
 // POST /api/signin
