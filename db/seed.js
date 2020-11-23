eb3c6c13e45bfa1133cdc429d3a91e6d9678d2e1
shanestarkweather <shane@shanestarkweather.com>
2020-11-23 12:55:15 -0800

    User auth connect to fe.

--- db/seed.js
+++ db/seed.js
@@ -3,33 +3,35 @@ const seedData = require('./galleries.json');
 const User = require('../models/user');
 
 const getUser = async () => {
-  try {
-    if (!process.argv[2]) {
-      throw new Error(
-        'To seed the database provide an email address for an existing user'
-      );
-    }
-    const user = await User.findOne({ email: process.argv[2] });
-    if (!user) {
-      throw new Error('No matching user found!');
-    }
-    return user;
-  } catch (error) {
-    console.error(error);
-  }
+	console.log(process.argv);
+	try {
+		if (!process.argv[2]) {
+			throw new Error(
+				'To seed the database provide an email address for an existing user'
+			);
+		}
+		const user = await User.findOne({ email: process.argv[2] });
+		if (!user) {
+			throw new Error('No matching user found!');
+		}
+		return user;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 Gallery.deleteMany()
+
   .then(getUser)
-  .then((user) => {
-    const seedDataWithOwner = seedData.map((gallery) => {
-      gallery.owner = user._id;
-      return gallery;
-    });
-    return Gallery.insertMany(seedDataWithOwner);
-  })
-  .then(console.log)
-  .then(console.error)
-  .finally(() => {
-    process.exit();
-  });
+	.then((user) => {
+		const seedDataWithOwner = seedData.map((gallery) => {
+			gallery.owner = user._id;
+			return gallery;
+		});
+		return Gallery.insertMany(seedDataWithOwner);
+	})
+	.then(console.log)
+	.then(console.error)
+	.finally(() => {
+		process.exit();
+	});
